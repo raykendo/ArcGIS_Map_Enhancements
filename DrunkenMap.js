3e7e0948c5f4e532805d882a36109feeef3c954e
Ken Doman <kdoman@bruceharris.com>
2015-04-01 12:26:07 -0500

    Drunken Map now works with LeafletJS

--- DrunkenMap.js
+++ DrunkenMap.js
@@ -1,21 +1,16 @@
 (function () {
 	var head = document.head || document.getElementsByTagName("head")[0],
 		css = [
-			".map .container .layersDiv .layerTile, .map .container .layersDiv>div { -webkit-transition: -webkit-transform",
+			" .leaflet-zoom-anim .leaflet-zoom-animated, .leaflet-container, .leaflet-tile-container, .leaflet-map-pane, .leaflet-tile-pane, .map .container .layersDiv>div>div, .map .container .layersDiv .layerTile, .map .container .layersDiv>div { -webkit-transition: -webkit-transform",
 			", transform",
 			", width",
 			", height",
 			"; transition: -webkit-transform",
 			", transform",
+			", opacity",
 			", width",
 			", height",
-			";} .map .container .layersDiv>div>div {-webkit-transition: opacity",
-			", transform",
-			", -webkit-transform",
-			" !important; transition: opacity",
-			", transform",
-			", -webkit-transform",
-			" !important; }"
+			";}"
 		].join(" 3s ease 500ms"),
 		style = document.createElement("style");
 	style.type = "text/css";
